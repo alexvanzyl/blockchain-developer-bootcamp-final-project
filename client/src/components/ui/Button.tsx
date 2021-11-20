15be5677f4530053081ede1107255f249710e7a4
Alexander van Zyl <vanzyl.alex@gmail.com>
2021-11-20 14:39:49 +0200

    Add size variants for button

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -3,6 +3,7 @@ type Props = {
   type: "button" | "submit";
   className?: string | undefined;
   variant?: string;
+  size?: "xs" | "sm" | "md" | "lg" | "xl";
   disabled?: boolean;
   onClick?: () => void;
 };
@@ -11,6 +12,7 @@ const Button = ({
   children,
   className,
   variant = "green",
+  size = "md",
   ...rest
 }: Props): JSX.Element => {
   const variants: Record<string, string> = {
@@ -19,10 +21,18 @@ const Button = ({
       "text-white bg-yellow-600 hover:bg-yellow-700 focus:ring-yellow-500",
   };
 
+  const sizes: Record<string, string> = {
+    xs: "px-3 py-1.5 text-xs",
+    sm: "px-3.5 py-2 text-sm",
+    md: "px-4 py-2 text-sm",
+    lg: "px-5 py-2 text-base",
+    xl: "px-6 py-3 text-base",
+  };
+
   return (
     <button
       {...rest}
-      className={`${className} ${variants[variant]} relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2`}
+      className={`${className} ${variants[variant]} ${sizes[size]} relative inline-flex items-center border border-transparent font-medium rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2`}
     >
       <span>{children}</span>
     </button>
